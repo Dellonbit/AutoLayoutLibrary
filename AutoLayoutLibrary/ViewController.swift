eeb6b0995e3a7a27352b55f40f47ba163e42e055
Richard Della <gtchdella@gmail.com>
2019-01-27 16:00:25 -0600

    Initial Commit

---
+++ AutoLayoutLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutLibrary
+//
+//  Created by richie on 2019-01-27.
+//  Copyright © 2019 richie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
