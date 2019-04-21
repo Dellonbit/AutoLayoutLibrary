d5df9424d4ddd200ac01810344291a7e373830ba
Richard Della <gtchdella@gmail.com>
2019-04-20 23:21:17 -0600

    add code for centery

--- AutoLayoutLibrary/LayoutLib.swift
+++ AutoLayoutLibrary/LayoutLib.swift
@@ -44,7 +44,9 @@ extension UIView {
     }
      //center X only
     func centerY(uv:UIView, top: CGFloat, leading: CGFloat, width: CGFloat, height: CGFloat) {
-        
+        self.centerXAnchor.constraint(equalTo: uv.leadingAnchor, constant: 10).isActive = true
+        self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        self.heightAnchor.constraint(equalToConstant: height).isActive = true
     }
 
 }
